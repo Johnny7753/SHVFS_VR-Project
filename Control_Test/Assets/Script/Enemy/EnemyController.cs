8bb23abc217eb8d97d3ba165c078f58a8d4fd25b
RebeccaWangcnz <101112197+RebeccaWangcnz@users.noreply.github.com>
2023-04-06 18:39:46 +0800

    敌人随机生成 多敌人寻路

--- Control_Test/Assets/Script/Enemy/EnemyController.cs
+++ Control_Test/Assets/Script/Enemy/EnemyController.cs
@@ -9,11 +9,11 @@ public enum EnemyType
 };
 public class EnemyController : MonoBehaviour
 {
+    [Header("Attrabutes")]
     public float HP;
     public EnemyType type;
-    [Tooltip("player")]
-    [SerializeField]
-    private Transform player;
+
+    [Header("Enemy Movement")]
     [Tooltip("enemy chase goal")]
     [SerializeField]
     private Transform goal;
@@ -21,38 +21,82 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private Vector2 waitSeconds=new Vector2(5,10);
 
+    [Header("EnemyAttack")]
+    [SerializeField]
+    private Transform shootPoint;
+    [SerializeField]
+    private GameObject bullet;
+
+    private Transform player;
     private NavMeshAgent agent;
     private List<EnemyHidenPoint> points;
     private int nextPointIndex;
-
+    //make sure invoke run once
+    private bool hasFoundNextPoint;
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();        
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("FakePlayer").transform; //'GameObject.Find' need to be replaced
 
         //find first hide point
         points = EnemySystem.Instance.points;
-        nextPointIndex = Random.Range(0, points.Count);
-        Debug.Log(points[nextPointIndex]);
+        do
+        {
+            nextPointIndex = Random.Range(0, points.Count);
+        } while (points[nextPointIndex].isTaken);
+        points[nextPointIndex].isTaken = true;
         goal = points[nextPointIndex].transform;
         agent.destination = goal.position;
-        //update next point every 5 seconds
-        InvokeRepeating("FindNextPosition",5,5);
+        
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EnemyShoot();
+        }
     }
     private void FixedUpdate()
     {
-        //Debug.Log(Vector3.Distance(transform.position, points[nextPointIndex].transform.position));
-       if(Vector3.Distance(transform.position, points[nextPointIndex].transform.position)<1.5f)
+       //look at player to attack
+        if(Vector3.Distance(transform.position, goal.position) <1.5f)
        {
             transform.LookAt(player);
-       }
+            if (!hasFoundNextPoint)
+            {
+                Invoke("FindNextPosition", Random.Range(waitSeconds.x, waitSeconds.y));
+                hasFoundNextPoint = true;
+            }
+
+        }
     }
 
     private void FindNextPosition()
     {
+        int lastIndex = nextPointIndex;
         nextPointIndex = Random.Range(nextPointIndex, points.Count);
+        if(points[nextPointIndex].isTaken)
+        {
+            nextPointIndex = lastIndex;
+            hasFoundNextPoint = false;
+            return;
+        }
+        points[lastIndex].isTaken = false;
+        points[nextPointIndex].isTaken = true;
         goal = points[nextPointIndex].transform;
         agent.destination = goal.position;
-
+        hasFoundNextPoint = false;
+    }
+    //destroy this enemy
+    public void EnemyDie()
+    {
+        points[nextPointIndex].isTaken = false;
+        Destroy(gameObject);
+    }
+    private void EnemyShoot()
+    {
+       var newBullet= Instantiate(bullet,shootPoint.position,shootPoint.rotation);
+       newBullet.GetComponent<Rigidbody>().AddForce(5000*transform.forward);
     }
 }
--- Control_Test/Assets/Script/Enemy/EnemySystem.cs
+++ Control_Test/Assets/Script/Enemy/EnemySystem.cs
@@ -4,21 +4,69 @@ using UnityEngine;
 
 public class EnemySystem : Singleton<EnemySystem>
 {
+    [Tooltip("just to show dont need to set values")]
     public List<EnemyHidenPoint> points;
+    [SerializeField]
+    private List<GameObject> enemyAlive;
+    [Tooltip("the boundary of sea, x-> min x, y->max x, z-> min y w-> max y")]
+    [SerializeField]
+    private Vector4 bornBoundary;
+    [Tooltip("the time for fresh new enemies")]
+    [SerializeField]
+    private int freshTimeInterval;
+    [Tooltip("the enemy's number range for refreshing")]
+    [SerializeField]
+    private Vector2 numRangeToFresh;
+    [SerializeField]
+    private GameObject enemyPrefab;
+
     public override void Awake()
     {
         base.Awake();
+        //get and sort all hidden point
         EnemyHidenPoint[] _points = FindObjectsOfType<EnemyHidenPoint>();
         points = new List<EnemyHidenPoint>(_points);
         SortPointByX();
+        enemyAlive = new List<GameObject>();
+        //fresh enemies
+        RefreshEnemies();
     }
+    private void Update()
+    {
+            //some cheat key
+            //kill all enemy
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            foreach (var enemy in enemyAlive)
+            {
+                enemy.GetComponent<EnemyController>().EnemyDie();
+            }
+            enemyAlive.Clear();
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            RefreshEnemies();
+        }
+    }
+
     //give points an order by x from smallest to biggest
     public void SortPointByX()
     {
         points.Sort((x,y)=>(x.transform.position.x.CompareTo(y.transform.position.x)));
-        //foreach (var point in points)
-        //{
-        //    Debug.Log(point.transform.position);
-        //}
+    }
+
+    private void RefreshEnemies()
+    {
+        int numToFresh = (int)Random.Range(numRangeToFresh.x, numRangeToFresh.y);
+        for (int i = 0; i < numToFresh; i++)
+        {
+            RefreshOneEnemy();
+        }
+    }
+    private void RefreshOneEnemy()
+    {
+        Vector3 position = new Vector3(Random.Range(bornBoundary.x,bornBoundary.y),0, Random.Range(bornBoundary.z, bornBoundary.w));
+        GameObject newEnemy= Instantiate(enemyPrefab,position,Quaternion.Euler( Vector3.right));
+        enemyAlive.Add(newEnemy);
     }
 }
