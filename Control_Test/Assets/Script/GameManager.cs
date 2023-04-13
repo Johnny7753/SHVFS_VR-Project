f6be66c705d8d087c5d9b09f2e99f1fc1333e601
shisiyuan14 <1375246695@qq.com>
2023-04-13 15:16:03 +0800

    UI Scene Setting & Combination of Enemy test Scene and Sample Scene
    UIStartScene test, enemy navmesh merge into Sample Scene have bugs.

--- Control_Test/Assets/Script/GameManager.cs
+++ Control_Test/Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,11 +17,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Isdead)
+        if(Isdead&&EndUI!=null)
         {
             Debug.Log("Dead!");
             EndUI.SetActive(true);
             Time.timeScale = 0;
         }
+
+        
+    }
+
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(1);
+
     }
 }
